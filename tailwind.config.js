1bea00fa8009ba6e83d5d1fe493f3d6916640d7f
Georgie Conway <dopestantelope@gmail.com>
2022-11-14 14:41:41 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  "./public/*.{html,js,css}",
+  "./views/*.ejs",
+  "./views/layouts/*.ejs",
+  "./views/partials/*.ejs"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
